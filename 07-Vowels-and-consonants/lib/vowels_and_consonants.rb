c4ff0a9ce9c10f3d5055282b7c27bf9e8be8164f
Piotr <flissek@gmail.com>
2018-02-24 12:47:45 +0000

    add answer to the 7. task

--- 07-Vowels-and-consonants/lib/vowels_and_consonants.rb
+++ 07-Vowels-and-consonants/lib/vowels_and_consonants.rb
@@ -1,4 +1,8 @@
 def vowels_and_consonants(first_letter, second_letter)
   # TODO: return arrays of vowels and consonants included between first_letter and second_letter
-
+  array = (first_letter..second_letter).to_a
+  all_vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+  vowels = array & all_vowels
+  consonants = array - all_vowels
+  [vowels, consonants]
 end
