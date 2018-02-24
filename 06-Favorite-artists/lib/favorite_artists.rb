f094c6e11ea84341195ee25d1198512fa5384a34
Piotr <flissek@gmail.com>
2018-02-24 12:15:24 +0000

    add answer to the 6. task

--- 06-Favorite-artists/lib/favorite_artists.rb
+++ 06-Favorite-artists/lib/favorite_artists.rb
@@ -1,4 +1,4 @@
 def favorite_artists(artists, your_favorite_artist)
   # TODO: return an array of 3 artists with your favorite in first place
-
+  [your_favorite_artist, artists.first, artists[1]]
 end
